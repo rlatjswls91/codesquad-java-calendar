c08b78e47ece89737001b06a74fca2edc1f6844b
KIMSUNJIN <KIMSUNJIN@DESKTOP-MRBDADL>
2019-12-16 21:51:49 +0900

    get Max Day Of Month

--- src/sunjinkim/calendar/Calendar.java
+++ src/sunjinkim/calendar/Calendar.java
@@ -1,9 +1,19 @@
 package sunjinkim.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
-
+	public final static int[] MAX_DAYS_OF_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
 	public static void main(String[] args) {
-		System.out.println("Hello Calendar");
+		System.out.println("달을 입력하세요");
+		Scanner scanner = new Scanner(System.in);
+		int month = scanner.nextInt();
+		System.out.printf("%d월의 마지막 날은 %d일 입니다.", month, Calendar.getMaxDayOfMonth(month));
+		scanner.close();
 	}
-
+	
+	public static int getMaxDayOfMonth(int month) {
+		return MAX_DAYS_OF_MONTH[month-1];
+	}
 }
